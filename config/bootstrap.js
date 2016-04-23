024b95ec762deb8de6f35af78dde14a8f772eb27
James Chen <jam.chen@mac.com>
2016-04-24 02:01:34 +0800

    Add passport middlewares for view routes

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -8,9 +8,20 @@
  * For more information on bootstrapping your app, check out:
  * http://sailsjs.org/#!/documentation/reference/sails.config/sails.config.bootstrap.html
  */
+var passport = require('passport');
 
 module.exports.bootstrap = function(cb) {
 
+	passport.serializeUser(function (user, next) {
+		next(null, user.id);
+	});
+
+	passport.deserializeUser(function (id, next) {
+		return sails.models.user.findOne(id).then(function (user) {
+			next(null, user || null);
+			return user;
+		})['catch'](next);
+	});
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
   cb();
