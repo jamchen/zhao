cae8d121e39f987c02f083bc4d7de3be7d28a355
James Chen <jam.chen@mac.com>
2016-04-23 21:34:49 +0800

    set up routes

--- config/routes.js
+++ config/routes.js
@@ -34,7 +34,13 @@ module.exports.routes = {
 
   '/': {
     view: 'homepage'
-  }
+  },
+  'get /login': { view: 'user/login' },
+  'get /signup': { view: 'user/signup' },
+  '/welcome': { view: 'user/welcome' },
+  'post /login': 'UserController.login',
+  'post /signup': 'UserController.signup',
+  '/logout': 'UserController.logout'
 
   /***************************************************************************
   *                                                                          *
