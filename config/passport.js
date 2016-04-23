29ea7348787aba562e7e75cf0411c99e5893f75c
James Chen <jam.chen@mac.com>
2016-04-23 21:33:20 +0800

    set up passport

--- config/passport.js
+++ config/passport.js
@@ -13,3 +13,34 @@ passport.deserializeUser(function(id, done) {
     done(err, user);
   });
 });
+
+
+var LocalStrategy = require('passport-local').Strategy;
+// Build our strategy and register it w/ passport
+// (unfortunately the direct pass-it-in-to-authenticate usage
+//  no longer works in the latest version of passport on npm)
+passport.use(new LocalStrategy(
+	function(username, password, done) {
+		User.findOne({ username: username }, function (err, user) {
+			if (err) { return done(err); }
+			if (!user) {
+				return done(null, false, { message: 'Incorrect username.' });
+			}
+			console.log('user.password:', user.password, password);
+			if (user.password !== password) {
+				return done(null, false, {
+					message: 'Invalid Password'
+				});          
+			} else {
+				var returnUser = {
+					username: user.username,
+					createdAt: user.createdAt,
+					id: user.id
+				};
+				return done(null, returnUser, {
+					message: 'Logged In Successfully'
+				});
+			}
+		});
+	}
+));
