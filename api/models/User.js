529fc74d0c96fa913483ae28ea931e26130c4948
James Chen <jam.chen@mac.com>
2016-04-30 22:58:12 +0800

    resolve lodash version issue and clean up debug log

--- api/models/User.js
+++ api/models/User.js
@@ -1,4 +1,3 @@
-var _ = require('lodash');
 var _super = require('sails-auth/api/models/User');
 
 _.merge(exports, _super);
@@ -23,30 +22,20 @@ _.merge(exports, {
     },
 
     toJSON: function toJSON() {
-      sails.log.debug('user.toJSON ');
       var user = _super.attributes.toJSON.apply(this);
       user.facebookProfilePicture = this.getFacebookProfilePicture();
-      sails.log.debug('user.toJSON ', user);
       return user;
     },
 
     getFacebookProfilePicture: function getFacebookProfilePicture() {
-      sails.log.debug('user passports', this.passports);
-      // var facebookPassport = _.find(this.passports, { provider: 'facebook' });
-      var facebookPassport = _.find(this.passports, function(o) { 
-        sails.log.debug('passport', o);
-        return 'facebook' === o.provider; 
-      });
-      sails.log.debug('facebookPassport', facebookPassport);
+      var facebookPassport = _.find(this.passports, _.matches({ provider: 'facebook' }));
       if (facebookPassport && facebookPassport.identifier) {
-      sails.log.debug('return');
         return 'https://graph.facebook.com/v2.6/'+facebookPassport.identifier+'/picture';
       }    
     },
   },
 
   beforeCreate: function beforeCreate(user, next) {
-    sails.log.debug('user.beforeCreate', user);
     if (_super.beforeCreate) {
       _super.beforeCreate.apply(_super, arguments);
     }
