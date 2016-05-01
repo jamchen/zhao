d7838befa846d41809e9e5581d9d5c67eaff9893
James Chen <jam.chen@mac.com>
2016-05-01 17:21:38 +0800

    Add missing pet detail view

--- api/controllers/MissingPetController.js
+++ api/controllers/MissingPetController.js
@@ -8,6 +8,22 @@
 module.exports = {
 	post: function(req, res) {
 		return res.view();
+	},
+
+	detail: function(req, res) {
+		sails.log.debug('detail', req.param('id'));
+		MissingPet.findOne({id: req.param('id')})
+		.exec(function(err, missingPet) {
+			if (err) {
+				return res.serverError(err);
+			}
+			if (!missingPet) {
+				return res.notFound();				
+			}
+			res.view('missingpet/detail', {
+				missingPet: missingPet
+			});
+		});
 	}
 };
 
