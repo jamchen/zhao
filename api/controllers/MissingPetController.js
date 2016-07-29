b4fe219a6b56199c5ac706ff4b2847f04f10a340
James Chen <jam.chen@mac.com>
2016-07-29 22:25:33 +0800

    use co to write async call

--- api/controllers/MissingPetController.js
+++ api/controllers/MissingPetController.js
@@ -7,53 +7,71 @@
 
 var moment = require('moment');
 var _ = require('lodash');
+var co = require('co');
 
 module.exports = {
-	post: function(req, res) {
-		return res.view();
-	},
+    post: function(req, res) {
+        return res.view();
+    },
 
-	detail: function(req, res) {
-		moment.locale(req.getLocale());
-		MissingPet.findOne({id: req.param('id')})
-		.exec(function(err, missingPet) {
-			if (err) {
-				return res.serverError(err);
-			}
-			if (!missingPet) {
-				return res.notFound();				
-			}
-			res.view('missingpet/detail', {
-				missingPet: missingPet,
-				moment: moment
-			});
-		});
-	},
+    detail: function(req, res) {
+        moment.locale(req.getLocale());
+        MissingPet.findOne({ id: req.param('id') })
+            .exec(function(err, missingPet) {
+                if (err) {
+                    return res.serverError(err);
+                }
+                if (!missingPet) {
+                    return res.notFound();
+                }
+                res.view('missingpet/detail', {
+                    missingPet: missingPet,
+                    moment: moment
+                });
+            });
+    },
 
-	master: function (req, res) {
-		var criteria = {
-			skip: 0,
-			limit: 10,
-			sort: 'updatedAt DESC'
-		};
-		_.assign(criteria, {
-			skip: req.param('offset')||criteria.skip,
-			limit: req.param('limit')||criteria.limit,
-		});
-		moment.locale(req.getLocale());
-		MissingPet.find(criteria)
-		.exec(function(err, missingPets) {
-			if (err) {
-				return res.serverError(err);
-			}
-			if (!missingPets) {
-				return res.notFound();				
-			}
-			res.view('missingpet/master', {
-				missingPets: missingPets,
-				moment: moment
-			});
-		});
-	}
+    _getMissingPetsAsync: function(criteria) {
+        return new Promise(function(fulfill, reject) {
+            MissingPet.find(criteria)
+                .exec(function(err, missingPets) {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        fulfill(missingPets);
+                    }
+                });
+        });
+    },
+
+    master: function(req, res) {
+        var missingPetController = this;
+        var criteria = {
+            skip: 0,
+            limit: 10,
+            sort: 'updatedAt DESC'
+        };
+        _.assign(criteria, {
+            skip: req.param('offset') || criteria.skip,
+            limit: req.param('limit') || criteria.limit,
+        });
+        moment.locale(req.getLocale());
+
+        co(function*() {
+            var queryDB = yield {
+            	missingPets: missingPetController._getMissingPetsAsync(criteria)
+            };
+            if (!queryDB.missingPets) {
+            	return res.notFound();
+            }
+            res.view('missingpet/master', {
+            	missingPets: queryDB.missingPets,
+            	moment: moment
+            });            
+        }).catch(function(err) {
+            res.serverError(err);
+        });
+
+
+    }
 };
-
