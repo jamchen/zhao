cbeca4b19e9d43b1b76d5adcd7a629af22b74a50
EasonLin <jack2872@gmail.com>
2016-09-29 18:29:53 +0800

    Use html template (handlebars) to create new page

--- assets/js/view_specific/missingpet/master.js
+++ assets/js/view_specific/missingpet/master.js
@@ -1,11 +1,13 @@
 function runMasterJs(options) {
 	
+	var cardHandlerUrl = '/js/view_specific/cardHtmlTemplate.js';
+
 	var dc = options;
 	var offset = options.offset;
 	var limit = options.limit;
 	var total = options.total;
 	var totalPage = Math.ceil(total/10);
-
+	var beginningIdInPage = total - offset;
 	moment.locale(navigator.language || navigator.userLanguage);
 
 	if(offset < 0) {
@@ -15,12 +17,14 @@ function runMasterJs(options) {
 	}
 
 	//function for hide spinner when img onload	
-	hideSpinner();
+	hideSpinner(beginningIdInPage);
 
-	function hideSpinner() {
-		for (var k = 0; k < 10; k++) {
-			$('#master-picture' + k).hide();
-			checkImgStatus(k);
+	function hideSpinner(beginningIdInPage) {
+		for (var k = beginningIdInPage; k > (beginningIdInPage - 11); k--) {
+			(function(id) {
+				$('#master-picture' + id).hide();
+				checkImgStatus(id);
+			})(k)	
 		}
 	}
 
@@ -44,7 +48,6 @@ function runMasterJs(options) {
 				offset -= 10;
 			}
 			getAjax(offset);
-			console.log('offsetPre = ' + offset);
 		}	
 	}
 
@@ -59,7 +62,6 @@ function runMasterJs(options) {
 				offset += 10;
 			}
 			getAjax(offset);
-			console.log('offsetNext = ' + offset);
 		}
 	}
 
@@ -81,92 +83,209 @@ function runMasterJs(options) {
 
 	//get AJAX for peges
 	function getAjax(offset) {
-		var xhttp = new XMLHttpRequest();
+		var xhttp = getRequestObject();
 		var requestUrl = '/missingPet?limit=10&skip=' + offset + '&sort=updatedAt%20DESC';
 		xhttp.onreadystatechange = function() {
 			if(this.readyState == 4 && this.status == 200) {
-  				document.getElementById("row").innerHTML = createNewCardByAjax(JSON.parse(this.responseText));
-  				paginationClassesForNewPage(offset);
-  				hideSpinner();
+  				// document.getElementById("row").innerHTML = createNewCardByAjax(JSON.parse(this.responseText));
+  				// console.log('this', JSON.parse(this.responseText)[0]);
+  				var htmlContent = JSON.parse(this.responseText);
+  				// var source = $("#ajax-comment").html();
+				// var template = Handlebars.compile(source);
+				// var source2 = getCardHandlerTemplate();
+				// console.log('source', source2);
+				// var template2 = Handlebars.compile(source2);
+				getCardHandlerTemplate(htmlContent);
+				// replace new cards into DOM
+  				// for(var i = 0; i < htmlContent.length; i++) {
+  				// 	(function(cardNum) {
+  				// 		if(cardNum === 0) {
+  				// 			$('#row').html(template2(htmlContent[cardNum]));
+  				// 		} else {
+  				// 			$("#row").append(template2(htmlContent[cardNum]));
+  				// 		}
+  				// 	})(i);
+  				// }
+
+  				// document.getElementById("row").innerHTML = createNewCardByAjax(JSON.parse(this.responseText));
+  				
 				}
 			};
-			xhttp.open("GET", requestUrl, true);
-			xhttp.send();
+		xhttp.open("GET", requestUrl, true);
+		xhttp.send(null);
 	}
 
+	function getCardHandlerTemplate(htmlContent) {
+		var templateContent = getRequestObject();
+		templateContent.onreadystatechange = function() {
+			if(this.readyState == 4 && this.status == 200) {
+				console.log(this.responseText);
+				var sorce = this.responseText;
+				var template = Handlebars.compile(this.responseText);
+				for(var i = 0; i < htmlContent.length; i++) {
+  					(function(cardNum) {
+  						if(cardNum === 0) {
+  							$('#row').html(template(htmlContent[cardNum]));
+  						} else {
+  							$("#row").append(template(htmlContent[cardNum]));
+  						}
+  					})(i);
+  				}
+  				paginationClassesForNewPage(offset);
+  				hideSpinner(total - offset);
+			}
+		}
+		templateContent.open("Get", cardHandlerUrl, true);
+		templateContent.send(null);
+	}
+
+
+	// function getTemplete(cardHandlerUrl) {
+	// 	var source = getRequestObject();
+	// 	source.onreadystatechange = function() {
+
+	// 	}
+	// }
+
+	// function handleResponse(xhttp,
+ //                        	cardHandlerUrl) {
+	// if((xhttp.readyState == 4) &&
+	//   (xhttp.status == 200)) {
+	//     	var htmlContent = JSON.parse(xhttp.responseText);
+	//     	$("#row").html(template(htmlContent[0]) + xhttp.responseText);
+	//     	console.log(xhttp.responseText);
+	//       	paginationClassesForNewPage(offset);
+	// 		hideSpinner()
+	//   }
+	// }
+
+	// function handleResponse(request,
+ //                        responseHandler,
+ //                        isJsonResponse) {
+	// 	if ((request.readyState == 4) &&
+	// 	   (request.status == 200)) {
+
+	// 	    // Default to isJsonResponse = true
+	// 	    if (isJsonResponse == undefined) {
+	// 	    	isJsonResponse = true;
+	// 	    }
+
+	// 	    if (isJsonResponse) {
+	// 	    	responseHandler(JSON.parse(request.responseText));
+	// 	    }
+	// 	    else {
+	// 	    	responseHandler(request.responseText);
+	// 	    }
+	// 	  }
+	// 	}
+
+
+	function getRequestObject() {
+	  if (window.XMLHttpRequest) {
+	    return (new XMLHttpRequest());
+	  } 
+	  else if (window.ActiveXObject) {
+	    // For very old IE browsers (optional)
+	    return (new ActiveXObject("Microsoft.XMLHTTP"));
+	  } 
+	  else {
+	    // global.alert("Ajax is not supported!");
+	    return(null); 
+	  }
+	}
 
 	//create new cards by AJAX
-	function createNewCardByAjax(missingPets) {
-		var cardHtml = '';
-		for(var j = 0; j < missingPets.length; j++) {
-			cardHtml += '<div class="col s12 m6">';
-				cardHtml += '<div class="col m12 s12 card">';
-					cardHtml += '<a target="_blank" href="/missingPet/detail/' + missingPets[j].id + ' ">';
-						cardHtml += '<div class="card-image waves-effect waves-block waves-light">';
-							cardHtml += '<div id="mainPicture' + j + '">';
-								cardHtml += '<div id="loader' + j + '" class="loader">';
-							 	cardHtml += '<img id="master-picture' + j + '" class="activator master-picture" src="' + missingPets[j].mainPictureUrl + '">';
-							 	cardHtml += '</div>';
-							cardHtml += '</div>';
-							cardHtml += '<span class="card-title activator master-name col s12 grey image-title">';
-								cardHtml += '名稱：'+ missingPets[j].name;
-								cardHtml += '<i class="material-icons right">more_vert</i>';
-							cardHtml += '</span>';
-						cardHtml += '</div>';
-					cardHtml += '</a>';
+	// function createNewCardByAjax(missingPets) {
+	// 	var cardHtml = '';
+	// 	for(var j = 0; j < missingPets.length; j++) {
+	// 		cardHtml += '<div class="col s12 m6">';
+	// 			cardHtml += '<div class="col m12 s12 card">';
+	// 				cardHtml += '<a target="_blank" href="/missingPet/detail/' + missingPets[j].id + ' ">';
+	// 					cardHtml += '<div class="card-image waves-effect waves-block waves-light">';
+	// 						cardHtml += '<div id="mainPicture' + j + '">';
+	// 							cardHtml += '<div id="loader' + j + '" class="loader">';
+	// 						 	cardHtml += '<img id="master-picture' + j + '" class="activator master-picture" src="' + missingPets[j].mainPictureUrl + '">';
+	// 						 	cardHtml += '</div>';
+	// 						cardHtml += '</div>';
+	// 						cardHtml += '<span class="card-title activator master-name col s12 grey image-title">';
+	// 							cardHtml += '名稱：'+ missingPets[j].name;
+	// 							cardHtml += '<i class="material-icons right">more_vert</i>';
+	// 						cardHtml += '</span>';
+	// 					cardHtml += '</div>';
+	// 				cardHtml += '</a>';
 
 					
-					cardHtml += '<div class="card-content flow-text">';
-						cardHtml += '<div class="col s5 column1 truncate">';
-							cardHtml += '<p class="master-kind">';
-								cardHtml += '類別：' + missingPets[j].kind;
-							cardHtml += '</p>';
-							cardHtml += '<p class="master-breed">';
-								cardHtml += '品種：' + missingPets[j].breed;
-							cardHtml += '</p>';
-							cardHtml += '<p class="master-color">';
-								cardHtml += '花色：' + missingPets[j].color;
-							cardHtml += '</p>';
-							cardHtml += '<p class="master-gender">';
-								var petGender = missingPets[j].gender
-								if(petGender == 'male') {
-									petGender = '公'
-								} else if(petGender == 'female') {
-									petGender = '母'
-								}
-								cardHtml += '性別：' + petGender;
-							cardHtml += '</p>';
-						cardHtml += '</div>';
-						cardHtml += '<div class="col s7 column2">';
-							cardHtml += '<p class="master-location truncate">';
-								cardHtml += '走失地點：' + missingPets[j].location;
-							cardHtml += '</p>';
-							cardHtml += '<p class="master-missingDate">';
-								var date = missingPets[j].missingDate;
-								cardHtml += '走失日期：' + moment(date).format('L');
-							cardHtml += '</p>';
-							cardHtml += '<p class="master-mostDistantFuture">';
-								var createdDate = missingPets[j].createdAt;
-								var updatedDate = missingPets[j].updatedAt;
-								//var mostDistantFuture = moment().max(createdDate, updatedDate);
-								if (updatedDate !== '' || updatedDate !== null || updatedDate !== undefined) {
-									var mostDistantFuture = updatedDate;
-								} else {
-									var mostDistantFuture = createdDate;
-								}
-								cardHtml += '更新日期：' + moment(mostDistantFuture).format('L');
-							cardHtml += '</p>';
-						cardHtml += '</div>';
-					cardHtml += '</div>';
-				cardHtml += '</div>';
-			cardHtml += '</div>';
-		}
-		// console.log('total = ' + total);
-		// console.log('offset = ' + offset);
-		// console.log('limit = ' + limit);
-		return cardHtml
-	}
+	// 				cardHtml += '<div class="card-content flow-text">';
+	// 					cardHtml += '<div class="col s5 column1 truncate">';
+	// 						cardHtml += '<p class="master-kind">';
+	// 							cardHtml += '類別：' + missingPets[j].kind;
+	// 						cardHtml += '</p>';
+	// 						cardHtml += '<p class="master-breed">';
+	// 							cardHtml += '品種：' + missingPets[j].breed;
+	// 						cardHtml += '</p>';
+	// 						cardHtml += '<p class="master-color">';
+	// 							cardHtml += '花色：' + missingPets[j].color;
+	// 						cardHtml += '</p>';
+	// 						cardHtml += '<p class="master-gender">';
+	// 							var petGender = missingPets[j].gender
+	// 							if(petGender == 'male') {
+	// 								petGender = '公'
+	// 							} else if(petGender == 'female') {
+	// 								petGender = '母'
+	// 							}
+	// 							cardHtml += '性別：' + petGender;
+	// 						cardHtml += '</p>';
+	// 					cardHtml += '</div>';
+	// 					cardHtml += '<div class="col s7 column2">';
+	// 						cardHtml += '<p class="master-location truncate">';
+	// 							cardHtml += '走失地點：' + missingPets[j].location;
+	// 						cardHtml += '</p>';
+	// 						cardHtml += '<p class="master-missingDate">';
+	// 							var date = missingPets[j].missingDate;
+	// 							cardHtml += '走失日期：' + moment(date).format('L');
+	// 						cardHtml += '</p>';
+	// 						cardHtml += '<p class="master-mostDistantFuture">';
+	// 							var createdDate = missingPets[j].createdAt;
+	// 							var updatedDate = missingPets[j].updatedAt;
+	// 							//var mostDistantFuture = moment().max(createdDate, updatedDate);
+	// 							if (updatedDate !== '' || updatedDate !== null || updatedDate !== undefined) {
+	// 								var mostDistantFuture = updatedDate;
+	// 							} else {
+	// 								var mostDistantFuture = createdDate;
+	// 							}
+	// 							cardHtml += '更新日期：' + moment(mostDistantFuture).format('L');
+	// 						cardHtml += '</p>';
+	// 					cardHtml += '</div>';
+	// 				cardHtml += '</div>';
+	// 			cardHtml += '</div>';
+	// 		cardHtml += '</div>';
+	// 	}
+	// 	// console.log('total = ' + total);
+	// 	// console.log('offset = ' + offset);
+	// 	// console.log('limit = ' + limit);
+	// 	return cardHtml;
+	// }
 
+
+	// Handlebars helper for cards
+	// translate gender from EN to ZH-TW 
+	Handlebars.registerHelper("genderToChinses", function(gender){
+		if(gender == 'male') {
+			return '公';
+		}
+		else if(gender == 'female') {
+			return '母';
+		}		
+	});
+
+	Handlebars.registerHelper("localDateFormat", function(date){
+		return moment(date).format('L');
+	});
+
+	Handlebars.registerHelper("lastUpdatedDate", function(createdAt, updatedAt){
+		var mostDistantFuture = moment().max(createdAt, updatedAt);
+		return moment(mostDistantFuture).format('L');
+	});
 	
 
 	//function to change classes to fit new page
